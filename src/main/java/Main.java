cd23b9358c0683c6bb94c76164952b78765aaba5
Stefan Andersson <stefan.andersson@sveki.se>
2026-01-18 21:56:25 +0100

    Add user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String args[]) {
         String[] weekdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        System.out.println("Select a number between 1 and 7!");
+        Scanner scanner = new Scanner(System.in);
+        int userNumber = scanner.nextInt();
+        while(userNumber < 1 || userNumber > 7) {
+            System.out.println("No! The number must be between 1 and 7!");
+            userNumber = scanner.nextInt();
+        }
+
+
+
     }
+
+
+
+
 
 }
